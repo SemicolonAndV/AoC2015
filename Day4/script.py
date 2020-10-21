d7f4120e31d90b41d47bd411cf86b85f4ed2ab49
Kuba <kubapaj00@gmail.com>
2020-10-21 15:24:07 +0200

    Solved day 4

---
+++ Day4/script.py
@@ -0,0 +1,26 @@
+import hashlib
+
+with open('Day4/input.txt', 'r') as file:
+    data = file.read()
+    
+index = 0
+end_1 = '00000'
+
+while True:
+    result = data + str(index)
+    if hashlib.md5(result.encode()).hexdigest()[:5] == end_1:
+        break
+    index += 1
+    
+print(f'Part 1: {index}')
+
+index = 0
+end_2 = '000000'
+
+while True:
+    result = data + str(index)
+    if hashlib.md5(result.encode()).hexdigest()[:6] == end_2:
+        break
+    index += 1
+    
+print(f'Part 2: {index}')
